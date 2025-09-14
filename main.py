477ca949db28139233fc5716c7bf18b211850e79
lijiabao1 <3533440766@qq.com>
2025-09-14 14:27:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,8 @@
-rate=6
-usd=100
-cny=usd*rate
-print(f"{usd}美元={cny}人民币")
-import turtle
-turtle.setup(650, 350, 200, 200)
-turtle.penup()
-turtle.fd(-250)
-turtle.pendown()
-turtle.pensize(25)
-colors = ["purple", "blue", "green", "yellow", "orange", "red"]
-for i in range(4):
-    turtle.pencolor(colors[i % len(colors)])
-    turtle.seth(-40)
-    turtle.circle(40, 80)
-    turtle.circle(-40, 80)
-turtle.pencolor(colors[4 % len(colors)])
-turtle.circle(40, 80/2)
-turtle.fd(40)
-turtle.pencolor(colors[5 % len(colors)])
-turtle.circle(16, 180)
-turtle.fd(40 * 2 / 3)
-turtle.done()
-import turtle
-turtle.seth(0)
-turtle.fd(100)
-turtle.seth(120)
-turtle.fd(100)
-turtle.seth(240)
-turtle.fd(100)
-import turtle
-turtle.speed(0)
-length=5
-for i in range(100):
-    turtle.fd(length)
-    turtle.right(90)
-    length+=5
-turtle.done()
- 
+weight = input().strip()  
+
+if weight == "10kg":
+    print("对应的英制重量为22.046磅")
+elif weight == "10pd":
+    print("对应的公制重量为4.535公斤")
+else:
+    print("输入错误")
